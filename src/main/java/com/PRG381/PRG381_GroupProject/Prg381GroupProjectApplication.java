eef154a31c0c3d2139941df1fece7b5bdb02a6fc
Frosslee <91604209+Frosslee@users.noreply.github.com>
2022-08-24 21:13:00 +0200

    awe

---
+++ src/main/java/com/PRG381/PRG381_GroupProject/Prg381GroupProjectApplication.java
@@ -0,0 +1,13 @@
+package com.PRG381.PRG381_GroupProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Prg381GroupProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Prg381GroupProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/PRG381/PRG381_GroupProject/Prg381GroupProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PRG381.PRG381_GroupProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Prg381GroupProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
