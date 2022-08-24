e45b5c79f00ba121d88822e8a0649d307781fcfd
Frosslee <91604209+Frosslee@users.noreply.github.com>
2022-08-24 21:05:42 +0200

    Springboot files

---
+++ src/main/java/com/PRG381/demo/Prg381GroupProjectApplication.java
@@ -0,0 +1,13 @@
+package com.PRG381.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Prg381GroupProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Prg381GroupProjectApplication.class, args);
+	}
+
+}
